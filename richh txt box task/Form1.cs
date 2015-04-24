3149dfbd49be0f2d1a39c3c2c7d6c25855777636
Syed-Faizan-Ali <alifaizan00@gmail.com>
2015-04-24 09:18:50 +0500

    my first repo test

---
+++ richh txt box task/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace richh_txt_box_task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.LoadFile( " C:/Users/user/Desktop/rtbf.txt",RichTextBoxStreamType.PlainText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SaveFile(" C:/Users/user/Desktop/rtbf.txt", RichTextBoxStreamType.PlainText);
+        }
+    }
+}
